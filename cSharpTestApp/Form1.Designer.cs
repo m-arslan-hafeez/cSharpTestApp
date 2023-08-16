ffafde0a55d0c00563af1939f30d845b04b8a376
Administrator <Administrator@PC-AA>
2023-08-16 19:20:58 +0500

    Add project files.

---
+++ cSharpTestApp/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace cSharpTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.textBoxOutputPath = new System.Windows.Forms.TextBox();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonFile = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "file_name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "key";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "output_file_path";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(178, 73);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(207, 20);
+            this.textBoxFile.TabIndex = 4;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(178, 115);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(207, 20);
+            this.textBoxAddress.TabIndex = 5;
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(178, 155);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(207, 20);
+            this.textBoxKey.TabIndex = 6;
+            // 
+            // textBoxOutputPath
+            // 
+            this.textBoxOutputPath.Location = new System.Drawing.Point(178, 198);
+            this.textBoxOutputPath.Name = "textBoxOutputPath";
+            this.textBoxOutputPath.Size = new System.Drawing.Size(207, 20);
+            this.textBoxOutputPath.TabIndex = 7;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(178, 246);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncrypt.TabIndex = 8;
+            this.buttonEncrypt.Text = "Encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonFile
+            // 
+            this.buttonFile.Location = new System.Drawing.Point(391, 92);
+            this.buttonFile.Name = "buttonFile";
+            this.buttonFile.Size = new System.Drawing.Size(40, 23);
+            this.buttonFile.TabIndex = 9;
+            this.buttonFile.Text = "File";
+            this.buttonFile.UseVisualStyleBackColor = true;
+            this.buttonFile.Click += new System.EventHandler(this.buttonFile_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(391, 198);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(40, 23);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 332);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonFile);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.textBoxOutputPath);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.TextBox textBoxOutputPath;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonFile;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ cSharpTestApp/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace cSharpTestApp
+{
+    public partial class Form1 : Form
+    {
+
+        int user_key;
+        string output_path;
+        string fileName;
+        string selectedFile;
+
+        [DllImport("EncDecLibrary.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void encryptFile(string file_name, string address, int key, string output_file_path);
+        [DllImport("EncDecLibrary.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void decryptFile(string file_path, string address, int key, string output_file_path);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "All Files (*.*)|*.*";
+                openFileDialog.Multiselect = false;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFile = openFileDialog.FileName;
+                    fileName = Path.GetFileName(selectedFile);                     
+                    textBoxFile.Text = fileName;
+                    textBoxAddress.Text = selectedFile;
+
+                }
+            }
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            user_key = int.Parse(textBoxKey.Text);
+            output_path = textBoxOutputPath.Text;
+            //MessageBox.Show("File : " +fileName + " Address : " + selectedFile + " Key : " + user_key + " Output Path : " + output_path);
+            decryptFile(fileName, selectedFile, user_key, output_path);
+
+            MessageBox.Show("OKAY!");
+            //OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "All Files|*.*";
+            //openFileDialog.Title = "Select File to Encrypt";
+            //if (openFileDialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    string inputFile = openFileDialog.FileName;
+            //    textBoxFile.Text = inputFile;
+            //    string address = textBoxAddress.Text;
+            //    SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //    saveFileDialog.Filter = "Encrypted Files|*.enc";
+            //    saveFileDialog.Title = "Save Encrypted File As";
+            //    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            //    {
+            //        string encryptedFile = saveFileDialog.FileName;
+            //        int user_key = int.Parse(textBoxKey.Text);
+            //        MessageBox.Show("File encrypted and saved successfully!");
+            //    }
+            //}
+        }
+    }
+}
